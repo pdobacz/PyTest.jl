c831cc95e6613004c33833ef6288766de9cf38ba
Piotr Dobaczewski <pdobacz@gmail.com>
2016-09-04 23:39:15 +0200

    moved tests to tests - doesn't work now, fixing..

--- src/PyTest.jl
+++ src/PyTest.jl
@@ -42,27 +42,4 @@ macro pytest(test_function)
 end
 
 
-
-
-counter = Dict("counter" => 0)
-
-@fixture f function() counter["counter"] += 1 end
-@fixture g function(f) "dupa"*string(f) end
-@fixture h function(f, g) "dupa"*string(f)*g end
-
-@pytest function(f, g, h)
-  println("hello", f, g)
-  println("bye", h)
-end
-
-@pytest function(f, g, h)
-  println("fdffd", f, g)
-  println("errewerw", h)
-end
-
-@pytest function()
-  println("no args")
-end
-
-
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,25 @@ module tst
 using PyTest
 using Base.Test
 
+
+counter = Dict("counter" => 0)
+
+@fixture f function() counter["counter"] end
+@fixture g function(f) "dupa"*string(f) end
+@fixture h function(f, g) "dupa"*string(f)*g end
+
+@pytest function(f, g, h)
+  println("hello", f, g)
+  println("bye", h)
+end
+
+@pytest function(f, g, h)
+  println("fdffd", f, g)
+  println("errewerw", h)
+end
+
+@pytest function()
+  println("no args")
+end
+
 end

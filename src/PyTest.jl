50d09102f79a930a9543765f4ed72b90e5105b4f
Piotr Dobaczewski <pdobacz@gmail.com>
2016-09-07 22:23:18 +0200

    handful of docstrings

--- src/PyTest.jl
+++ src/PyTest.jl
@@ -5,13 +5,29 @@ include("exceptions.jl")
 export @fixture, @pytest,
        PyTestException
 
+"Helper type holding information about a fixture definition"
 type Fixture
   s::Symbol
   f::Function
-  fargs::Array{Symbol, 1}
-  fixtures_dict
+  fargs::Array{Symbol, 1}  # all dependency-fixtures' symbols
+  fixtures_dict  # maps dependency-fixtures' symbols to their resp. Fixtures
 end
 
+"""
+Defines a fixture, that's called on every call to `@pytest` and calls
+its set of dependency-fixtures.
+
+Usage:
+```
+@fixture fixture_name function(other_fixture1, other_fixture2)
+  # other_fixture1 holds the result of dependency other_fixture1
+  do_sth_with(other_fixture1)
+  do_sth_with(other_fixture2)
+  # ...
+  return fixture_results
+end
+```
+"""
 macro fixture(s, fixture_function)
   typeof(s) == Symbol || throw(ArgumentError("s must be an indentifier"))
   fixture_function.head in [:function, :->] || throw(ArgumentError("fixture_function should be a function"))
@@ -20,11 +36,27 @@ macro fixture(s, fixture_function)
   fargs, escfargs = get_fixtures_from_function(fixture_function)
   return quote
     fixtures = $escfargs
+
+    # gather all dependency-fixtures from this fixture
     fixtures_dict = Dict{Symbol, Fixture}(zip($fargs, fixtures))
+
+    # build the Fixture instance and assign to the given variable
     $(esc(s)) = Fixture($(string(s)), $(esc(fixture_function)), $fargs, fixtures_dict)
   end
 end
 
+"""
+Defines a single test, that calls its depenency-fixtures
+
+Usage:
+```
+@pytest function(fixture1, fixture2)
+  # fixture1 holds result of call to fixture1 etc
+  do_sth_with(fixture1)
+  # ... rest of test
+end
+```
+"""
 macro pytest(test_function)
   test_function.head in [:function, :->] || throw(ArgumentError("test_function should be a function"))
   test_function.args[1].head == :call && throw(ArgumentError("test_function should be anonymous"))
@@ -33,19 +65,26 @@ macro pytest(test_function)
   return quote
     fixtures = $escfargs
 
+    # empty collection of fixtures' results
     results = Dict{Symbol, Any}()
+
+    # go through all fixtures used (recursively) and evaluate
     farg_results = [get_fixture_result(f, results) for f in fixtures]
+
     $(esc(test_function))(farg_results...)
   end
 end
 
+# helpers
 
+"Convenience function to extract information from `@pytest` `@fixture` call"
 function get_fixtures_from_function(f)
   fargs = [farg for farg in f.args[1].args[1:end]]
   escfargs = Expr(:vect, [esc(farg) for farg in fargs]...)
   fargs, escfargs
 end
 
+"Convenience function to call a single fixture, after all dependencies are called"
 function get_fixture_result(fixture::Fixture, results::Dict{Symbol, Any})
   if fixture.s in keys(results)
     return results[fixture.s]

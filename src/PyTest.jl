9605525d55f7a914d41e48fc12077dd07bc5dae0
Piotr Dobaczewski <pdobacz@gmail.com>
2016-09-07 22:12:29 +0200

    refactoring fixtures

--- src/PyTest.jl
+++ src/PyTest.jl
@@ -17,19 +17,33 @@ macro fixture(s, fixture_function)
   fixture_function.head in [:function, :->] || throw(ArgumentError("fixture_function should be a function"))
   fixture_function.args[1].head == :call && throw(ArgumentError("fixture_function should be anonymous"))
 
-  fargs = [farg for farg in fixture_function.args[1].args[1:end]]
-  fixture_symbol = esc(s)
-  s_name = string(s)
-  escfargs = [esc(farg) for farg in fargs]
-  escfargs2 = Expr(:vect, escfargs...)
+  fargs, escfargs = get_fixtures_from_function(fixture_function)
   return quote
-    fixtures = $escfargs2
-    fixtures_dict = Dict{Symbol, Fixture}()
-    for (s, f) in zip($fargs, fixtures)
-      fixtures_dict[s] = f
-    end
-    $fixture_symbol = Fixture($s_name, $(esc(fixture_function)), $fargs, fixtures_dict)
+    fixtures = $escfargs
+    fixtures_dict = Dict{Symbol, Fixture}(zip($fargs, fixtures))
+    $(esc(s)) = Fixture($(string(s)), $(esc(fixture_function)), $fargs, fixtures_dict)
   end
+end
+
+macro pytest(test_function)
+  test_function.head in [:function, :->] || throw(ArgumentError("test_function should be a function"))
+  test_function.args[1].head == :call && throw(ArgumentError("test_function should be anonymous"))
+
+  fargs, escfargs = get_fixtures_from_function(test_function)
+  return quote
+    fixtures = $escfargs
+
+    results = Dict{Symbol, Any}()
+    farg_results = [get_fixture_result(f, results) for f in fixtures]
+    $(esc(test_function))(farg_results...)
+  end
+end
+
+
+function get_fixtures_from_function(f)
+  fargs = [farg for farg in f.args[1].args[1:end]]
+  escfargs = Expr(:vect, [esc(farg) for farg in fargs]...)
+  fargs, escfargs
 end
 
 function get_fixture_result(fixture::Fixture, results::Dict{Symbol, Any})
@@ -41,22 +55,5 @@ function get_fixture_result(fixture::Fixture, results::Dict{Symbol, Any})
   results[fixture.s] = new_result
   new_result
 end
-
-macro pytest(test_function)
-  test_function.head in [:function, :->] || throw(ArgumentError("test_function should be a function"))
-  test_function.args[1].head == :call && throw(ArgumentError("test_function should be anonymous"))
-
-  fargs = [farg for farg in test_function.args[1].args[1:end]]
-  escfargs = [esc(farg) for farg in fargs]
-  escfargs2 = Expr(:vect, escfargs...)
-  return quote
-    fixtures = $escfargs2
-
-    results = Dict{Symbol, Any}()
-    farg_results = [get_fixture_result(f, results) for f in fixtures]
-    $(esc(test_function))(farg_results...)
-  end
-end
-
 
 end # module

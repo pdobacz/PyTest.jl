cf6ddf97bc9eb67fb95e650cde8034646072dc10
Piotr Dobaczewski <pdobacz@gmail.com>
2016-09-13 22:59:39 +0200

    using basetestnext

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,14 @@ include("different_module.jl")
 module tst
 
 using PyTest
-using Base.Test
+
+# see https://github.com/JuliaCI/BaseTestNext.jl
+if VERSION >= v"0.5.0-dev+7720"
+    using Base.Test
+else
+    using BaseTestNext
+    const Test = BaseTestNext
+end
 
 include("base_fixtures.jl")
 include("builtin/tempdir.jl")

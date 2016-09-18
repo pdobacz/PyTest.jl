8f0b5d2af8e644849090a5d38093db5904d52f6d
Piotr Dobaczewski <pdobacz@gmail.com>
2016-09-18 17:16:48 +0200

    some failing tests

--- test/basetest_integration.jl
+++ test/basetest_integration.jl
@@ -4,3 +4,36 @@ let
     @test 1 == 1
   end
 end
+
+# skip & broken
+# FIXME: this requires stuff from Base.Test which is not in BaseTestNext
+let
+  result = true
+  @pytest function skip_here()
+    @test_skip result = false
+  end
+  @test result
+end
+
+let
+  result = true
+  @test_skip @pytest function skip_this()
+    result = false
+  end
+end
+
+let
+  @pytest function broken_here()
+    @test_broken false
+  end
+end
+
+let
+  result = true
+  @test_broken @pytest function broken_this()
+    @test false
+    @test result = false
+  end
+  @test result == false
+end
+# end skip & broken

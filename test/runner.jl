2f63cb892a9f9d6a59c11ce48ae260219e9daf0f
Piotr Dobaczewski <pdobacz@gmail.com>
2016-10-11 23:09:55 +0200

    quick 0.5 compatibilitty for travis

--- test/runner.jl
+++ test/runner.jl
@@ -1,6 +1,11 @@
 runner_path = joinpath(Pkg.dir(), "PyTest/src/runner.jl")
 julia_exe = Base.julia_cmd()
-tests_count_indicator(passed, total) = "test set      |    $passed      $total"
+
+# FIXME: ugly way of making the test pass - come up with a way to really test test selection
+"Returns an expected output from Base.Test to check if tests are selected as expected"
+tests_count_indicator(passed, total) =
+  VERSION >= v"0.5" ? "test set    |    $passed      $total" :
+                      "test set      |    $passed      $total"
 
 cd(joinpath(Pkg.dir(), "PyTest/test/test_package")) do
   @test contains(readall(`$julia_exe $runner_path`),
